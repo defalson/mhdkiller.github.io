f0e469e3c9632a516bc7017f4fa23c7c3f7d8256
Defalson <defallahmhtsaleh42@gmail.com>
2024-05-04 12:37:01 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+
+const voir = document.getElementById('voir');
+const appari = document.querySelector('.apparie');
+
+voir.addEventListener('click', () => {
+    appari.style.display = "block";
+    
+});
